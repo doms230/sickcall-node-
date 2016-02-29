d8eed3a64c125d72b381e7e15fb55871abb08349
Dominic Smith <doms230@live.com>
2016-02-29 08:13:50 -0600

    add createMerchant methods

--- routes/merchants.js
+++ routes/merchants.js
@@ -35,22 +35,23 @@ router.post('/createMerchant', function (req, res) {
                 postal_code: req.body.postal_code,
                 state: req.body.state
             },
-            dob:{
-                day:req.body.day,
-                month:req.body.month,
-                year:req.body.year
-            }
-           /* business_tax_id: req.body.business_tax_id,
-            business_name: req.body.business_name,
-            first_name:req.body.first_name,
-            last_name:req.body.last_name,
-          //  ssn_last_4: req.body.ssn_last_4,
-            "type": req.body.type
-        },*/
+            dob: {
+                day: req.body.day,
+                month: req.body.month,
+                year: req.body.year
+            },
+            /* business_tax_id: req.body.business_tax_id,
+             business_name: req.body.business_name,
+             first_name:req.body.first_name,
+             last_name:req.body.last_name,
+             //  ssn_last_4: req.body.ssn_last_4,
+             "type": req.body.type
+             },*/
 
-       /* tos_acceptance: {
-            date: Math.floor(Date.now() / 1000),
-            ip: "108.198.43.126" // Assumes you're not using a proxy*/
+            tos_acceptance: {
+                date: Math.floor(Date.now() / 1000),
+                ip: "108.198.43.126" // Assumes you're not using a proxy
+            }
         }
 
     }, function(err, account) {
@@ -112,7 +113,7 @@ router.get('/updateMerchant', function (req, res) {
 
     "acct_17iMxGFJ6cArZFcQ",
     {
-      /*  business_name:"SGI",
+        business_name:"SGI",
         decline_charge_on:{
             avs_failure:true,
             cvc_failure:true
@@ -138,7 +139,7 @@ router.get('/updateMerchant', function (req, res) {
             last_name:"Smith",
             ssn_last_4: "1234",
             "type": "corporation"
-        },*/
+        },
 
         tos_acceptance: {
             date: Math.floor(Date.now() / 1000),
