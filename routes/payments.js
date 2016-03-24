07470b501927f8aac0e889303d46db5a047c4969
Dominic Smith <doms230@live.com>
2016-03-24 16:34:10 -0500

    added payment route

--- routes/payments.js
+++ routes/payments.js
@@ -7,12 +7,16 @@ var stripe = require("stripe")(
     "sk_test_HSpPMwMkr1Z6Eypr5MMldJ46"
 );
 
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
 router.post('/buyTickets', function(req, res){
 
     var stripeToken = request.body.stripeToken;
 
     var charge = stripe.charges.create({
-        amount: request.body.amount, // amount in cents, again
+        amount: stripeToken.amount, // amount in cents, again
         currency: "usd",
         source: stripeToken,
         description: "Example charge"
