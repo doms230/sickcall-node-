751432fdb81f1cc2bd28ecb1cf514ba7dde2868e
Dominic Smith <doms230@live.com>
2016-05-23 12:29:00 -0500

    updated home images

--- routes/merchants.js
+++ routes/merchants.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var router = express.Router();
 var stripe = require("stripe")(
-    "sk_live_wFw3B2lTIGPACNAsoGAH9bPO"
+    "sk_test_HSpPMwMkr1Z6Eypr5MMldJ46"
 );
 
         /* GET users listing. */
