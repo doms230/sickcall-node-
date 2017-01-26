6b8b9ef5d1ca96ae03ab18300543e5214129d3c6
Dom Smith <dom@hiikey.com>
2017-01-25 23:39:52 -0600

    fixed the loading user issue

--- routes/events.js
+++ routes/events.js
@@ -19,7 +19,7 @@ parse.initialize("O9M9IE9aXxHHaKmA21FpQ1SR26EdP2rf4obYxzBF", "bctRQbnLCvxRIHaJTk
     router.get('/', function (req, res, next) {
 
         var date = new Date();
-        console.log( "date: " + date.toString());
+        console.log( "date: " + date.toLocaleString());
         //store the cookie
        // res.cookie("cookie" , 'jaja')
 
