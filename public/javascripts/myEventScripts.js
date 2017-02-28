87a9d9999c31d542e351cfc09f00ff97692abdc5
Dom Smith <dom@hiikey.com>
2017-02-28 12:33:31 -0600

    adf

--- public/javascripts/myEventScripts.js
+++ public/javascripts/myEventScripts.js
@@ -2,41 +2,51 @@
  * Created by d_innovator on 2/28/17.
  */
 
+var parse = require("parse").Parse;
+parse.initialize("O9M9IE9aXxHHaKmA21FpQ1SR26EdP2rf4obYxzBF"); parse.serverURL = 'http://localhost:3000/parse';
+var currentUser = parse.User.current();
+
 $(function(){
 
-    var parse = require("parse").Parse;
-    parse.initialize("O9M9IE9aXxHHaKmA21FpQ1SR26EdP2rf4obYxzBF"); parse.serverURL = 'http://localhost:3000/parse';
-    var currentUser = parse.User.current();
-    
-    
-    
-    
+    loadEventInfo()
 
 });
 
-function loadUserEvents() {
-    //if (currentUser){
-    var User = parse.Object.extend("_User");
-    var query = new parse.Query(User);
-    query.get("wcbsnOpMwH", {
-        success: function(object) {
-            //console.log(object);
-            // The object was retrieved successfully.
-            var username = object.getUsername();
-            var displayName = object.get("DisplayName");
-            var image = (object.get("Profile").name())[0].src = object.get("Profile").url();
-            var number = object.get("phoneNumber");
+function loadEventInfo(){
+    var Posts = parse.Object.extend('Event');
+    var query = new parse.Query(Posts);
+    query.equalTo("userId", "M3fUHRH1TQ");
+    query.find({
+        success: function(results) {
+            // Do something with the returned Parse.Object values
+            for (var i = 0; i < results.length; i++) {
+                var object = results[i];
 
-            document.getElementById('username').value = username;
-            document.getElementById('inputName').value = displayName;
-            document.getElementById('inputNumber').value = number;
-            document.getElementById('image').src = image;
+                eventId = object.id;
+
+                var startDate = new Date(object.get("startTime"));
+                var endDate = new Date(object.get("endTime"));
+                //TODO: Change this
+                //var createJaunt = momenttz.tz(createDate, "America/Chicago").format("ddd, MMM Do YYYY, h:mm a");
+                eventTitle = object.get('title');
+                var image = (object.get("eventImage").name())[0].src = object.get("eventImage").url();
+                var code = object.get("code");
+                var description = object.get("description");
+                eventHostId = object.get("userId");
+                loadEventUser(eventHostId,eventTitle,image,code,description,startDate,endDate);
+
+                address = object.get("address");
+                var eventLocation = object.get('location');
+                coordinates = eventLocation.latitude + "," + eventLocation.longitude;
+                var invites = object.get('invites');
+
+                configureUser(invites);
+
+            }
         },
-        error: function(object, error) {
-            // The object was not retrieved successfully.
-            // error is a Parse.Error with an error code and message.
+        error: function(error) {
+            //alert("Error: " + error.code + " " + error.message);
+            //res.send("Error: " + error.code + " " + error.message);
         }
     });
-    //}
-    
 }
