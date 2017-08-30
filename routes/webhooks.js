fe68c7d06f84ccbb791b7347f478ce287a166361
Dom Smith <dom@doms-mac-mini.attlocal.net>
2017-08-29 23:21:54 -0500

    asdf

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -12,13 +12,15 @@ router.get('/', function(req, res, next) {
 
 router.post('/stripe', function(req, res, next){
 
-    let sig = req.headers["stripe-signature"];
-    let event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-    console.log(event);
+   // let sig = req.headers["stripe-signature"];
+   // let event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
     /*var event_json = JSON.parse(req.body);
     res.send(200);*/
+    var event_json = JSON.parse(req.body);
+    console.log(event_json)
 
-    res.json({received: true});
+    //res.json({received: true});
+    res.send(200);
 
  });
 
