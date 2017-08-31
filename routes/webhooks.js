9614958249f9632cfdc2eb747c0f4721280ea96b
Dom Smith <dom@doms-mac-mini.attlocal.net>
2017-08-30 21:24:38 -0500

    asdf

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -8,12 +8,13 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 var event_json; 
+var subject; 
+
 const msg = {
     to: 'dom@sickcallhealth.com',
     from: 'noreply@sickcallhealth.com',
-    subject: 'Sending with SendGrid is Fun',
-    text: event_json,
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    subject: subject,
+    text: event_json
   };
 
 router.get('/', function(req, res, next) {
@@ -26,6 +27,7 @@ router.post('/stripe', function(req, res, next){
    // let event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
     /*var event_json = JSON.parse(req.body);
     res.send(200);*/
+    subject = "Message from Stripe";
      event_json = req.body;
     console.log(event_json);
     sgMail.send(msg);
