4146e0f90df5c62e3edae33abf7e8a157e3236f0
Dominic Smith <doms230@live.com>
2017-12-29 16:18:04 -0600

    Update payments.js

--- routes/payments.js
+++ routes/payments.js
@@ -6,11 +6,11 @@ var express = require('express');
 var router = express.Router();
 var http = require('http');
 
-var stripe = require("stripe")("sk_live_i757TXWOABq1CfpbtQVsZZAv");
-var testStripe = require("stripe")("sk_test_XjgzLWe3uty249H9iZ6YtzId");
+var stripe = require("stripe")([]);
+var testStripe = require("stripe")([]);
 
 var parse = require("parse/node").Parse;
-parse.initialize("O9M9IE9aXxHHaKmA21FpQ1SR26EdP2rf4obYxzBF", "bctRQbnLCvxRIHaJTkv3gqhlwSzxjiMesjx8kEwo");
+parse.initialize([], []);
 
 router.get('/', function(req, res, next) {
     res.json({ 
